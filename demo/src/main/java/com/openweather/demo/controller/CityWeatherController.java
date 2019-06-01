febe102a8440bc64292121192f7d8aa4f17f1078
hairinwind <dong_yao@hotmail.com>
2019-06-01 09:49:51 -0700

    implemet ui seletion by populated list

--- demo/src/main/java/com/openweather/demo/controller/CityWeatherController.java
+++ demo/src/main/java/com/openweather/demo/controller/CityWeatherController.java
@@ -1,8 +1,12 @@
 package com.openweather.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,22 +19,33 @@ import com.openweather.demo.util.WeatherAdapter;
 @Controller
 public class CityWeatherController {
 	
+	// this is for demo purpose only
+	// ideally these data shall be saved in database
+	private static List<City> allCities = new ArrayList<City>();
+	static {
+		allCities.add(new City("london,uk"));
+		allCities.add(new City("hong kong,cn"));
+	}
+	
 	@Autowired
 	private WeatherService weatherService;
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	protected String index(Model model){
 		City city = new City();
 		model.addAttribute("city", city);
+		model.addAttribute("allCities", allCities);
 		return "weather";
 	}
 
 	@RequestMapping("/cityweather")
 	protected String getWeatherByCity(@ModelAttribute(value="city") City city, Model model){
-		OpenWeatherResult openWeatherResult = weatherService.getWeather(city.getName());
-		
-		Weather weather = WeatherAdapter.adaptToWeather(openWeatherResult);
-		model.addAttribute("weather", weather);
+		model.addAttribute("allCities", allCities);
+		if (city.getName() != null) {
+			OpenWeatherResult openWeatherResult = weatherService.getWeather(city.getName());		
+			Weather weather = WeatherAdapter.adaptToWeather(openWeatherResult);
+			model.addAttribute("weather", weather);
+		}
 		return "weather";
 	}
 }
